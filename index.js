010e92df46eb79d5f570f6ab9930e3c6a523e758
li.jon.seow.dev@gmail.com <li.jon.seow.dev@gmail.com>
2020-10-02 23:26:03 +0800

    initial commit with radius wiz

---
+++ index.js
@@ -0,0 +1,14 @@
+radiuswizard = ( options ) => {
+    let elements = document.querySelectorAll('.radiuswizard')
+
+    elements.forEach( el => {
+        if (options.circle){
+            el.style.borderRadius = '50%';
+        }
+        else{
+            el.style.borderRadius = '25px';
+        }
+    })
+}
+
+module.exports.radiuswizard = this.radiuswizard;
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Description 
+	npm package for adding border-radius to your elements.
+
+# Installation
+	`npm i radius -- save`
+	```
+
+	import {radius} from 'radiuswizard';
+	radiuswizard({
+		circle: false
+	})
+	```
+
+## Options 
+    radius wizard supports only one option:
+	- _circle_  - _boolean_ (Defaults to false)
